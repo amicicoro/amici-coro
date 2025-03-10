74bc668efde05c1751bcaed0fae3fe5ce0d261bf
Fay Adams <fay737731@googlemail.com>
2025-03-10 22:37:27 +0000

    Remove unused code

--- lib/db.ts
+++
@@ -1,7 +0,0 @@
-import { events } from "@/lib/events-data"
-
-export async function getEventBySlug(slug: string) {
-  const allEvents = [...events]
-  return allEvents.find((event) => event.slug === slug)
-}
-
