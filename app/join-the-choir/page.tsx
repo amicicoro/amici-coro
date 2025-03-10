642ac6e3d6ccce8ec673743ddff73e11cde58898
Fay Adams <fay737731@googlemail.com>
2025-03-10 21:03:36 +0000

    Renamed the component Button

--- app/join-the-choir/page.tsx
+++ app/join-the-choir/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/Button"
 import { Header } from "@/components/layout/Header"
 import { Footer } from "@/components/layout/Footer"
 
